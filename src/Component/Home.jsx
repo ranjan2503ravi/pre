df56b859167f1eba901991745573eb2dde96e9f1
ranjan2503ravi <ranjan2503ravi@gmail.com>
2026-01-30 08:12:38 +0530

    hii

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,18 +1,43 @@
 import React, { useContext } from 'react'
 import { MyContext } from '../Context/Context'
-const Home = () => {
-  const { Data, setData } = useContext(MyContext);
-  console.log(Data)
-  const handleClick = () => {
-    setData({name:"hello"})
-  }
+import { useState } from 'react'
+export const Home = () => {
+  const { Data, setData, onSent } = useContext(MyContext)
+  const [Input, setInput] = useState("")
+
+  const handle = () => {
+  onSent(Input)
+}
+
+
   return (
     <div>
-      {Data.name}
-<button onClick={handleClick}>click</button>
+     <div className="flex items-center gap-2 m-auto bg-white p-3 rounded-lg shadow-md w-full max-w-md">
+  <input
+    onChange={(e) => setInput(e.target.value)}
+    value={Input}
+    type="text"
+    placeholder="Enter the prompt..."
+    className="flex-1 px-4 py-2 border border-gray-300 rounded-md outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+  />
+  <button onClick={handle} className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">
+    Click
+  </button>
+      </div>
+     {Data && Data.length > 0 ? (
+  <div className="mt-6 grid grid-cols-2 md:grid-cols-3 gap-4">
+    {Data.map((item) => (
+      <div key={item.idMeal} className="border p-2 rounded shadow">
+        <img src={item.strMealThumb} alt={item.strMeal} />
+        <p className="mt-2 font-semibold">{item.strMeal}</p>
+      </div>
+    ))}
+  </div>
+) : (
+  <p className="text-center mt-6 text-gray-500">No data found</p>
+)}
+
 
     </div>
   )
 }
-
-export default Home
--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -1,11 +1,32 @@
+import axios from 'axios';
 import React, { createContext } from 'react'
 import { useState } from 'react';
+import { useEffect } from 'react';
 export const MyContext = createContext();
 const Context = ({ children }) => {
-    const [Data, setData] = useState({name:"ravi"})
+    const [Data, setData] = useState([]);
+    
+    const onSent = async (hello="Arrabiata") => {
+       try {
+           const Api = `https://www.themealdb.com/api/json/v1/1/search.php?s=${hello}`
+           const res = await axios.get(Api)
+           console.log(res.data.meals)
+           setData(res.data.meals)
+           
+       } catch (error) {
+        console.log(error)
+       }
+    }
+        
+    
+    
+    useEffect(() => {
+     onSent()
+    }, [])
+    
   return (
       <div>
-          <MyContext.Provider value={{Data,setData}}>
+          <MyContext.Provider value={{Data,setData,onSent}}>
               {children}
           </MyContext.Provider>
     </div>
